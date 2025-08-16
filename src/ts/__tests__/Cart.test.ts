7e4a04267c12aa093e087aaa5e2ebb5a3bb4afbc
Svetlana_Shcherbakova <shchalal@mail.ru>
2025-08-16 22:25:11 +0300

    no message

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,28 @@
 import Cart from '../service/Cart';
 
-test('new card should be empty', () => {
-  const cart = new Cart();
+import Movie from '../domain/Movie';
 
-  expect(cart.items.length).toBe(0);
+describe('Cart with Movie', () => {
+  test('adds movie once and calculates total', () => {
+    const cart = new Cart();
+    const movie = new Movie({
+      id: 77,
+      name: 'Интерстеллар',
+      originalTitle: 'Interstellar',
+      year: 2014,
+      country: 'США',
+      slogan: 'Mankind was born on Earth. It was never meant to die here.',
+      genres: ['фантастика', 'драма', 'приключения'],
+      duration: 169,
+      price: 350,
+    });
+
+    cart.add(movie);
+    cart.add(movie); 
+
+    expect(cart.items.length).toBe(1);
+    expect(cart.items[0]).toBe(movie);
+    expect(cart.total()).toBe(350);
+  });
 });
+
